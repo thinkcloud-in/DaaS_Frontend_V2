3956dd307cb0147fd103a37e82ed6eabff7c5064
kaibalyanayak <knayak@thinkcloud.in>
2026-01-22 15:13:32 +0530

    Ldap issue resolved

--- src/redux/features/Rbac/RbacUtils.js
+++ src/redux/features/Rbac/RbacUtils.js
@@ -70,6 +70,7 @@ export const baseNavigation = [
       { name: "SSL", href: "/ssl", componentKey: "SSL" },
       { name: "SMTP", href: "/SmtpConfig", componentKey: "SMTP" },
       { name: "RBAC", href: "/user_management", componentKey: "RBAC" },
+      { name: "Retention Period", href: "/retention-period", componentKey: "Retention Period" },
     
     ],
   },
