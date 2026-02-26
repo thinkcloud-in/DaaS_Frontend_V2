1641f8c3fb5b8fdedf3e28f8d459f8849a8aa770
rcvarpit31 <arpitsingh@thinkcloud.in>
2026-02-26 13:13:29 +0530

    update route

--- src/App.js
+++ src/App.js
@@ -210,7 +210,7 @@ function App() {
         <GrafanaToolbarContextProvider>
           {/* <PoolContextProvider token={refreshToken} tokenParsed={tokenParsed}> */}
             {/* <RBACProvider tokenParsed={tokenParsed} token={refreshToken}> */}
-              <BrowserRouter basename="/admin">
+              <BrowserRouter>
                 <ToastContainer />
                 <Sidebar tokenParsed={tokenParsed} />
                 <Suspense fallback={LoadingSpinner()}>
